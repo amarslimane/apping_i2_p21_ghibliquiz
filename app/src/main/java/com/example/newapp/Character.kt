71b2fb29943005e982c9ad6da07a40e2aac69267
amarslimane <as.amarslimane@gmail.com>
2019-10-29 13:16:21 +0100

    final_version

--- app/src/main/java/com/example/newapp/Character.kt
+++ app/src/main/java/com/example/newapp/Character.kt
@@ -1,4 +1,13 @@
 package com.example.newapp
 
-class Character {
-}
+data class Character (
+    val id: Int,
+    val name: String,
+    val gender: String,
+    val age: String,
+    val eye_color: String,
+    val hair_color: String,
+    val films: String,
+    val species: String,
+    val url: String
+)
